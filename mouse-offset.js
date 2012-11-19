300a907a74c4af8c44bcf871f2fcaa407c730453
Karl Böhlmark <karl.bohlmark@edgeware.tv>
2012-11-19 12:17:05 +0100

    include element offset in mouse offset

--- mouse-offset.js
+++ mouse-offset.js
@@ -1,4 +1,20 @@
-var elementOffset = require('./elementOffset');
+var elementOffset = function(elem) {
+  var x, y;
+  x = 0;
+  y = 0;
+  while (true) {
+    x += elem.offsetLeft;
+    y += elem.offsetTop;
+    if (!(elem = elem.offsetParent)) {
+      break;
+    }
+  }
+  return {
+    x: x,
+    y: y
+  };
+};
+
 var getOffset = function(event, elem) {
     var offset = elementOffset(elem);
     return {
